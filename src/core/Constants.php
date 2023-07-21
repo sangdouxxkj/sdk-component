af7cf1cf07ac227479f837fa3576358e2387284e
张兴煜 <zhangxingyu1@mail.3-marks.com>
2023-07-21 17:55:05 +0800

    feat: 补充常量

--- src/core/Constants.php
+++ src/core/Constants.php
@@ -6,4 +6,9 @@ class Constants
 {
     const ACCESS_TOKEN_COMPONENT = 'access_token_component'; // 三方平台token
     const ACCESS_TOKEN_ACCESS = 'access_token'; // 应用token
+    /**
+     * @description 微信事件类型
+     */
+    const EVENT_COMPONENT_VERIFY_TICKET = 'component_verify_ticket';// 验证票据
+
 }
